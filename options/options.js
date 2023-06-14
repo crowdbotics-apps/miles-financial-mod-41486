bcfa372623d0b0b45f745bca2943d3c621e1bf74
Crowdbotics <team@crowdbotics.com>
2023-06-14 19:17:46 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "miles-financial-mod-41486",
+  url: "https://miles-financial-mod-41486.botics.co",
+  api: "https://miles-financial-mod-41486.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
